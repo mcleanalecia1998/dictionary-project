eb8533d6d0a9bb284cffb361183c72cd1eb37160
mcleanalecia1998 <139597906+mcleanalecia1998@users.noreply.github.com>
2024-02-19 20:55:51 -0500

    Testing react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from "./logo.png";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <img src={logo} className="App-logo" alt="logo" />
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+      </div>
     </div>
   );
 }
